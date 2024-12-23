cf29e32c82367f41921e2c3b45a6e52c77977933
Daniel Reyes <86884359+DanielDevHN@users.noreply.github.com>
2024-12-23 10:37:20 -0600

    Update app

--- src/app.ts
+++ src/app.ts
@@ -1,58 +1,14 @@
-import "dotenv/config";
-import { addKeyword, createBot, createFlow, MemoryDB } from '@builderbot/bot'
-import { provider } from './providers/meta.provider'
-import {adapterDB, connectToMongoDB } from "./database/mongo.db";
-import { loadDynamicFlow } from "./flows/dynamic.flow";
-
-const PORT = process.env.PORT ?? 3003
-
+import { startServer } from "./server/server";
 
 const main = async () => {
+  try {
+    console.log("Inicializando el servidor del bot...");
+    await startServer();
+    console.log("Servidor iniciado correctamente.");
+  } catch (error: any) {
+    console.error("Error al iniciar el servidor:", error.message);
+    process.exit(1);
+  }
+};
 
-    try {
-        const db = await connectToMongoDB();
-
-        const dynamicFlowArray = await loadDynamicFlow(db);
-
-        const dynamicFlows = createFlow(dynamicFlowArray);
-        
-        const adapterProvider = provider;
-    
-        const { handleCtx, httpServer } = await createBot({
-            flow: dynamicFlows,
-            provider,
-            database: adapterDB,
-        })
-    
-        adapterProvider.server.post(
-            '/v1/register',
-            handleCtx(async (bot, req, res) => {
-                const { number, name } = req.body
-                await bot.dispatch('REGISTER_FLOW', { from: number, name })
-                return res.end('trigger')
-            })
-        )
-    
-        adapterProvider.server.post(
-            '/v1/flow',
-            handleCtx(async (bot, req, res) => {
-                try {
-                    const { number } = req.body
-                    await bot.dispatch('TEST', { from: number, name: 'bot' })
-                    return res.end('ok')
-    
-                } catch (error) {
-                    console.log(error)
-                    return res.end('error')
-                }
-            })
-        )
-    
-        httpServer(+PORT)
-        console.log(`🚀 Bot is running on port ${PORT}`);
-    } catch (error) {
-        console.error("❌ Error starting the bot:", error); 
-    }
-}
-
-main()
+main();
